6b4f511ffbab04dfc7db982dc8422be2e70fbfdd
daniveg20 <danielvegavera@gmail.com>
2022-10-11 08:35:21 +0200

    Actualización 10-10-2022

--- funciones_aux.py
+++ funciones_aux.py
@@ -14,18 +14,18 @@ def gen_transacciones():
     cuenta_Or   = random.randint(0,99999)
     cuenta_Dest = 65478
     cantidad    = random.randint(0,1000000)
-    cadena = str(cuenta_Or) + "_" + str(cuenta_Dest) + "_" + str(cantidad)
-    return cadena
+    nonce = gen_nonce()
+    cadena = str(cuenta_Or) + " " + str(cuenta_Dest) + " " + str(cantidad) + " " + str(nonce)
+    return cadena 
 
 # gen_prueba_random: 
 # Función que según probabilidad, modifica la transacción
 def gen_prueba_random(cadena):
     v = random.random()
-    if(v > 0.65):
-        x= cadena.split("_")
+    print(cadena)
+    print(v)
+    if(v > 0.00005):
+        x = cadena.split(" ")
         x[2] = random.randint(0,1000000)
-        cadena = x[0] + "_" + x[1] + "_" + str(x[2])
+        cadena = str(x[0]) + " " + str(x[1]) + " " + str(x[2]) + " " + str(x[3])
     return cadena
-
-cad = gen_transacciones()
-gen_prueba_random(cad)
